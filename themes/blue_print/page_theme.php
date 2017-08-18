7c2507946cb3118bf3f58c71f7357a8f83e3a53b
Michael <michael@unumotors.com>
2017-08-18 08:26:45 +0200

    add sample code

--- themes/blue_print/page_theme.php
+++ themes/blue_print/page_theme.php
@@ -1,7 +1,10 @@
 <?php
 namespace Concrete\Package\ThemeBluePrint\Theme\BluePrint;
 
-class PageTheme extends \Concrete\Core\Page\Theme\Theme
+use Concrete\Core\Area\Layout\Preset\Provider\ThemeProviderInterface;
+use Concrete\Core\Page\Theme\Theme;
+
+class PageTheme extends Theme implements ThemeProviderInterface
 {
     protected $pThemeGridFrameworkHandle = 'bootstrap3';
 
@@ -15,19 +18,83 @@ class PageTheme extends \Concrete\Core\Page\Theme\Theme
         return t('A basic theme for reuse');
     }
 
-    public function getThemeAreaClasses()
+    public function registerAssets()
     {
-        return array(
-            'Main' => array('specialArea'),
-            'Grid Container Two' => array('minorArea')
-        );
+        $this->providesAsset('css', 'bootstrap/*');
     }
 
     public function getThemeBlockClasses()
     {
-        return array(
-            'content' => array('specialBlock'),
-            '*' => array('minorBlock')
-        );
+        return [
+            /*'block_handle' => [
+                'class-name-one',
+                'class-name-two'
+            ],*/
+        ];
+    }
+
+    public function getThemeAreaClasses()
+    {
+        return [
+            /*'area name' => [
+                'area-class'
+            ],*/
+        ];
+    }
+
+    public function getThemeDefaultBlockTemplates()
+    {
+        return [
+             /*'block_handle' => 'default_template.php',*/
+        ];
+    }
+
+    public function getThemeResponsiveImageMap()
+    {
+        return [
+            /*'large' => '900px',*/
+        ];
+    }
+
+    public function getThemeEditorClasses()
+    {
+        return [
+            /*[
+                'title' => t('Editor Class Block'),
+                'menuClass' => 'css-class-for-menu',
+                'spanClass' => 'css-class-for-block',
+                'forceBlock' => '1'
+            ],
+            [
+                'title' => t('Editor Class Inline'),
+                'menuClass' => '',
+                'spanClass' => 'css-class-for-inline',
+                'forceBlock' => '-1'
+            ],*/
+        ];
+    }
+
+    public function getThemeAreaLayoutPresets()
+    {
+        return [
+            /*[
+                'handle' => 'left_sidebar',
+                'name' => 'Left Sidebar',
+                'container' => '<div class="row"></div>',
+                'columns' => [
+                    '<div class="col-sm-4"></div>',
+                    '<div class="col-sm-8"></div>',
+                ],
+            ],
+            [
+                'handle' => 'right_sidebar',
+                'name' => 'Right Sidebar',
+                'container' => '<div class="row"></div>',
+                'columns' => [
+                    '<div class="col-sm-8"></div>',
+                    '<div class="col-sm-4"></div>',
+                ],
+            ],*/
+        ];
     }
 }
